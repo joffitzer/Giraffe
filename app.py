dc2501d5d5f03490b446283a87cdf593408f4ef1
joffitzer <joffitzer@gmail.com>
2020-03-07 12:21:25 -0500

    simple built-in python functions

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 character_name = "Jim"
-character_age = "40"
+character_age = "50"
 
 print("There once was a man named " + character_name + ",")
 print("who was " + character_age + " years old.")
 print("He liked the name " + character_name + ",")
 print("but he didn't like being " + character_age + " years old.")
+
+phrase = "Learning Python"
+
+print(phrase)
+
+print("Is the phrase upper case?", phrase.isupper())
+
+print("Length of the phrase: ", len(phrase))
+
+print("Phrase at the 0th element: ", phrase[0])
+
+print("Find index in phrase: ", phrase.index("r"))
+
