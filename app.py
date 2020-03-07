34a0477298c5597eb7831bebe2b54c077901ee1b
joffitzer <joffitzer@gmail.com>
2020-03-07 11:53:41 -0500

    simple variable implementation

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-print("Hello World")
+character_name = "Jim"
+character_age = "40"
+
+print("There once was a man named " + character_name + ",")
+print("who was " + character_age + " years old.")
+print("He liked the name " + character_name + ",")
+print("but he didn't like being " + character_age + " years old.")
